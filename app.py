7e476e5044e2defbd691d91f379819cbd1056bc6
krtech26 <krtech26@gmail.com>
2025-03-09 10:54:03 +0530

    Second Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to My Basic Web App</h1><p>This is a simple Flask application.</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
